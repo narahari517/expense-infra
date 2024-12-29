f97896c5eb47302d9100ef6fbcc0b48741e99171
Narahari <narahari517@gmail.com>
2024-12-29 15:24:49 +0530

    jenkins infra dev

--- 00-vpc/provider.tf
+++ 00-vpc/provider.tf
@@ -2,12 +2,11 @@ terraform {
   required_providers {
     aws = {
       source = "hashicorp/aws"
-      version = "5.70.0"
+      version = "5.66.0"
     }
   }
 
   backend "s3" {
-    profile = "default"
     bucket = "aws-remote-state1-dev"
     key    = "expense-vpc-dev"
     region = "us-east-1"
