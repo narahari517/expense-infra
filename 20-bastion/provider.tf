afa13ba06491795991464b2aefe0e33c8264668e
Narahari <narahari517@gmail.com>
2024-12-30 19:46:15 +0530

    jenkins infra dev

---
+++ 20-bastion/provider.tf
@@ -0,0 +1,20 @@
+terraform {
+  required_providers {
+    aws = {
+      source = "hashicorp/aws"
+      version = "5.66.0"
+    }
+  }
+
+  backend "s3" {
+    bucket = "aws-remote-state1-dev"
+    key    = "expense-bastion"
+    region = "us-east-1"
+    dynamodb_table = "devops-locking-dev"
+  }
+}
+
+provider "aws" {
+  # Configuration options
+  region = "us-east-1"
+}
